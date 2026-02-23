c010c374ea4f5d0f36b51783dae3dafefbc5c225
Adriel Teles <adrielteles@outlook.com>
2026-02-23 02:05:45 -0300

    Adicionado exemplo de cabeçalho default para qualquer novo script em python

---
+++ python/cabecalho-exemplo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# cabecalho_exemplo.py - Exemplo de cabeçalho para novos scripts
+#
+# Site       : https://github.com/AdrielGTeles
+# Autor      : Adriel Teles <adrielteles@outlook.com>
+# Manutenção : Adriel Teles <adrielteles@outlook.com>
+#
+# ------------------------------------------------------------------------------
+#
+#   Este programa é apenas um exemplo de cabeçalho para novos scripts.
+#   Ele não realiza nenhuma função específica, mas serve como um modelo para
+#   a criação de novos scripts com um formato de cabeçalho consistente.
+#
+#
+#   Exemplos:
+#       $ python3 cabecalho_exemplo.py
+#       Saída:
+#
+# ------------------------------------------------------------------------------
+#
+#
+# Histórico de Versões:
+#
+#   Versão 1.0 2024-06-01, Adriel Teles:
+#     - Versão inicial do script.
+#
+#
+#   Licença: GPLv3
+#
+#
+# ------------------------------------------------------------------------------
+#
+
+def main():
+    """Função principal do script."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
